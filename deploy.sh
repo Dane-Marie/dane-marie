53cb82a8e72836b905aade6fbe45ac91d6f9829b
Simon Hjorthøj <sh2@live.dk>
2023-02-11 17:48:02 +0100

    deployment

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,4 @@ npm run generate
 npm install push-dir --save-dev
 npm run generate
 npm run deploy
-npm run build
-git add dist -f
-git commit -m "deployment"
-git subtree push --prefix dist origin gh-pages
+
