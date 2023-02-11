386323da2147cc53c6f376256a3b7e53a07144e4
Simon Hjorthøj <sh2@live.dk>
2023-02-11 18:27:48 +0100

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ export default {
     // Global page headers: https://go.nuxtjs.dev/config-head
     target: 'static',
     router: {
-        base: './portfolio'
+        base: '/portfolio/'
     },
     server: {
         host: (process.env.NODE_ENV === 'production' ? '0.0.0.0' : '127.0.0.1')
