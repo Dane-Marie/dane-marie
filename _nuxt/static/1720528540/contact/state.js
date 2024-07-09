e0db555364c4cbbc76892357430e2b78b61e7405
Dane-Marie <Dane-Marie@users.noreply.github.com>
2024-07-09 12:38:41 +0000

    deploy: d4375ba15da932d84427fa03702ba241ceddbb8f

--- _nuxt/static/1720528540/contact/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1720528540",layout:"default-layout",error:a,serverRendered:true,routePath:"\u002Fcontact",config:{_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
